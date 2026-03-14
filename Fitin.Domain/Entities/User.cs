d2fdda499443020b3e93af2fb0dd6f751db3f528
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2026-03-14 13:09:02 +0530

    Add soft delete query filters

--- Fitin.Domain/Entities/User.cs
+++ Fitin.Domain/Entities/User.cs
@@ -1,18 +1,17 @@
 using Fitin.Domain.Enums;
 using Fitin.Domain.Entities.CartItems;
 using Fitin.Domain.Entities.Wishlists;
+using Fitin.Domain.Common;
 
 namespace Fitin.Domain.Entities
 {
-    public class User
+    public class User : BaseEntity
     {
-        public Guid Id { get ; private set;}
         public string Name{get; private set;} =null!;
         public string Email {get; private set;} =null!;
         public string PasswordHash {get; private set;} =null!;
         public UserRole Role {get; private set;}
         public bool IsActive{get; private set;}
-        public DateTime CreatedAt{get; private set;}
 
         private readonly List<RefreshToken> _refreshTokens = new();
         public IReadOnlyCollection<RefreshToken> RefreshTokens => _refreshTokens.AsReadOnly();
@@ -23,13 +22,12 @@ namespace Fitin.Domain.Entities
 
         public User (string name, string email, string passwordHash ,UserRole role = UserRole.User)
         {
-            Id = Guid.NewGuid();
             Name = name;
             Email = email;
             PasswordHash = passwordHash;
             Role = role;
             IsActive = true;
-            CreatedAt = DateTime.UtcNow;
+            
         }
         public void AddRefreshToken(RefreshToken token)
         {
