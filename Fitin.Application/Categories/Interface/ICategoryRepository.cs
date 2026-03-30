4c67fd5e1d76369604b672bebf38d6724c64a36e
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2026-03-30 10:15:12 +0530

    Add Category repository interface

---
+++ Fitin.Application/Categories/Interface/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+
+
+using Fitin.Domain.Entities.Categories;
+
+namespace Fitin.Application.Categories.Interface;
+
+public class ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAllAsync();
+    Task<Category?> GetByIdAsync(Guid id);
+    Task<Category?> GetByNameAsync(string name);
+    Task AddAsync(Category category);
+    Task UpdateAsync(Category category);
+    Task DeleteAsync(Category category);
+}
