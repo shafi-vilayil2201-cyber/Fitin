a767ed91b508d0d557fdd591e2958a72ff58359a
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2026-04-01 11:23:34 +0530

    feat: add ImageUrl to Category and update database seeder

--- Fitin.Domain/Entities/Category.cs
+++ Fitin.Domain/Entities/Category.cs
@@ -5,17 +5,20 @@ namespace Fitin.Domain.Entities.Categories;
 public class Category : BaseEntity
 {
     public string Name { get; private set; } = string.Empty;
+    public string? ImageUrl {get;set;}
 
     private Category() { }
 
-    public Category(string name)
+    public Category(string name,string? imageUrl =null)
     {
         Name = name;
+        ImageUrl = imageUrl;
     }
 
-    public void UpdateName(string name)
+    public void UpdateName(string name,string? imageUrl)
     {
         Name = name;
+        ImageUrl = imageUrl;
         MarkUpdated();
     }
 }
