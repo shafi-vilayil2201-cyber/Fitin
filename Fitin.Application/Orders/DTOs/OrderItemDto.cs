718a0075ac4df07991519977c84cdb91bab28c16
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2026-04-02 12:34:14 +0530

    feat:add product image Urls to orders and display in profile

--- Fitin.Application/Orders/DTOs/OrderItemDto.cs
+++ Fitin.Application/Orders/DTOs/OrderItemDto.cs
@@ -4,6 +4,7 @@ public class OrderItemDto
 {
     public Guid ProductId { get; set; }
     public string ProductName { get; set; } = string.Empty;
+    public string ProductImageUrl{get;set;}= string.Empty;
     public decimal UnitPrice { get; set; }
     public int Quantity { get; set; }
     public decimal TotalPrice { get; set; }
