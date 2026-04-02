ecba29eb26f1f1dac9b57f008501f1ee411ed3fb
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2026-04-02 07:33:52 +0530

    feat: add shipping address fields to order entity

---
+++ Fitin.Application/Orders/DTOs/CreateOrderDto.cs
@@ -0,0 +1,10 @@
+namespace Fitin.Application.Orders.DTOs;
+
+public class CreateOrderDto
+{
+    public string ShippingName { get; set; } = string.Empty;
+    public string ShippingAddress { get; set; } = string.Empty;
+    public string ShippingCity { get; set; } = string.Empty;
+    public string ShippingPostalCode { get; set; } = string.Empty;
+    public string ShippingPhone { get; set; } = string.Empty;
+}
