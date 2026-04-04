969c4ac6ef81209e46b62eb7476067b208a370f7
shafi-vilayil2201-cyber <shafivilayil2201@gmail.com>
2026-04-04 07:08:26 +0530

    feat: enhance user & order DTOs and improve data mapping for admin insights

--- Fitin.Application/Orders/DTOs/OrderDto.cs
+++ Fitin.Application/Orders/DTOs/OrderDto.cs
@@ -1,9 +1,22 @@
+using Fitin.Application.Orders.DTOs;
+
 namespace Fitin.Application.Orders.DTOs;
 
 public class OrderDto
 {
     public Guid OrderId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string UserEmail { get; set; } = string.Empty;
     public decimal TotalAmount { get; set; }
     public string Status { get; set; } = string.Empty;
+    public DateTime OrderDate { get; set; }
+
+    // Shipping Details
+    public string ShippingName { get; set; } = string.Empty;
+    public string ShippingAddress { get; set; } = string.Empty;
+    public string ShippingCity { get; set; } = string.Empty;
+    public string ShippingPostalCode { get; set; } = string.Empty;
+    public string ShippingPhone { get; set; } = string.Empty;
+
     public List<OrderItemDto> Items { get; set; } = new();
 }
--- Fitin.Application/Users/DTOs/UserListItemDto.cs
+++ Fitin.Application/Users/DTOs/UserListItemDto.cs
@@ -1,4 +1,4 @@
-
+using System.Text.Json.Serialization;
 using Fitin.Domain.Enums;
 
 namespace Fitin.Application.Users.DTOs;
@@ -8,6 +8,8 @@ public class UserListItemDto
     public Guid Id{get; set;}
     public string Name{get; set;} = string.Empty;
     public string Email{get;set;} = string.Empty;
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public UserRole Role{ get;set;}
     public bool IsActive{get;set;}
     public DateTime CreatedAt{get;set;}
